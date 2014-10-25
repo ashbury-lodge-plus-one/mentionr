596f876f958fa0308936c7afa38d72307fd73d52
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-10-25 14:45:57 -0700

    dash service

--- client/app/dashboard/dashboard.service.js
+++ client/app/dashboard/dashboard.service.js
@@ -1,16 +1,45 @@
 'use strict';
 
+//Assume request
+
+
 angular.module('mentionrApp')
-  .factory('dashboard', function () {
+  .factory('dashboard', function ($http) {
     // Service logic
-    // ...
+    var populateWordsBar = function(user){
+      $http.get('/api/users/'+user).success(function(words){
+        return words;
+      })
+    }
 
-    var meaningOfLife = 42;
+    //Get Request for the Word Visualiser - Trigger on Click of Word from Left Bar
+    //Returned Object with x array corresponding y array and total length
+    var populateVisualizer = function(wordId){
+      $http.get('/api/words/'+wordId).success(function(stats){
+        var collateDates = {};
+        var output = {x: [], y: [], total: stats.length};
 
+        for (var i = 0; i < stats.length; i++) {
+          var temp = stats[i].date;
+          if (collateDates[temp] === undefined) {
+            collateDates[temp] = {count: 1, urls: [stats[i].url]};
+          } else {
+            collateDates[temp].count++;
+            collateDates[temp].urls.push(stats[i].url);
+          }
+        }
+        for (var date in collateDates){
+          output.x.push(date);
+          output.y.push(collateDates[date]);
+        }
+        return output;
+      })
+    }
+
+    
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      populateVisualizer: populateVisualizer,
+      populateWordsBar: populateWordsBar
       }
-    };
   });
