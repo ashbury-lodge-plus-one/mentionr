b04a05196f9991524a2df3180ff26898e01a993c
spencer handley <spencerhandley@gmail.com>
2014-10-25 21:28:54 -0700

    scaling

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -6,31 +6,9 @@ angular.module('mentionrApp')
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser();
 
-
-    $scope.allMentions = visualizationData.all;
-    if (visualizationData.x.length === 1) {
-      visualizationData.x.unshift('10/24/2014');
-      visualizationData.y.unshift('0');
-    }
-
     $scope.submit = function(){
       var userId = $scope.getCurrentUser.userId;
       dashboardFactory.submitWord($scope.wordToSubmit,userId);
     };
-
-  	$scope.WordChart = {
-      labels : visualizationData.x,
-      datasets : [
-        {
-          fillColor : 'rgba(151,187,205,0)',
-          strokeColor : '#FF6600',
-          pointColor : 'rgba(151,187,205,0)',
-          pointStrokeColor : '#e67e22',
-          data : visualizationData.y
-        }
-      ]
-  	};
-  	$scope.myChartOptions = {};
-  	$scope.vizData = visualizationData;
     $scope.message = 'Hello';
   });
---
+++ client/app/dashboard/word.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('mentionrApp')
+  .controller('WordCtrl', function ($location, $scope, visualizationData, Auth) {
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser();
+    $scope.words = Auth.getCurrentUser().words
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };    
+    $scope.allMentions = visualizationData.all
+    if (visualizationData.x.length === 1) {
+      visualizationData.x.unshift('10/24/2014')
+      visualizationData.y.unshift('0')
+    }
+
+    $scope.submit = function(){
+      var userId = $scope.getCurrentUser || 1;
+      console.log("word",$scope.wordToSubmit);
+      console.log("ID:",userId)
+      dashboardFactory.submitWord($scope.wordToSubmit,userId);
+    }
+
+  	$scope.WordChart = {
+      labels : visualizationData.x,
+      datasets : [
+        {
+          fillColor : 'rgba(151,187,205,0)',
+          strokeColor : '#FF6600',
+          pointColor : 'rgba(151,187,205,0)',
+          pointStrokeColor : '#e67e22',
+          data : visualizationData.y
+        }
+      ]
+  	};
+  	$scope.myChartOptions = {};
+  	$scope.vizData = visualizationData;
+    $scope.message = 'Hello';
+  });
