e8d2b3f9604e86ae33f33491291bc36c61271685
spencer handley <spencerhandley@gmail.com>
2014-10-26 00:19:15 -0700

    uncomment

--- server/config/seed.js
+++ server/config/seed.js
@@ -3,35 +3,35 @@
 var User = require('../api/user/user.model');
 var Word = require('../api/word/word.model');
 
-// User.find({}).remove(function() {
-//   User.create({
-//     provider: 'local',
-//     name: 'Test User',
-//     email: 'test@test.com',
-//     password: 'test'
-//   }, {
-//     provider: 'local',
-//     role: 'admin',
-//     name: 'Admin',
-//     email: 'admin@admin.com',
-//     password: 'admin',
-//     words: [
-//       {word: 'Google'},
-//       {word: 'Paypal'}
-//     ]
-//   }, function() {
-//       console.log('finished populating users');
-//     }
-//   );
-// });
+User.find({}).remove(function() {
+  User.create({
+    provider: 'local',
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+  }, {
+    provider: 'local',
+    role: 'admin',
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin',
+    words: [
+      {word: 'Google'},
+      {word: 'Paypal'}
+    ]
+  }, function() {
+      console.log('finished populating users');
+    }
+  );
+});
 
-// Word.find({}).remove(function() {
-//   Word.create({
-//     word: 'the',
-//     articles: []
+Word.find({}).remove(function() {
+  Word.create({
+    word: 'the',
+    articles: []
 
-//   }, function() {
-//       console.log('finished populating words');
-//     }
-//   );
-// });
+  }, function() {
+      console.log('finished populating words');
+    }
+  );
+});
