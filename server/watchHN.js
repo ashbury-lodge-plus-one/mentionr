497257446bbc2391394ceb45d07454a503ef641b
Josh Lankford <lankford.josh@gmail.com>
2014-10-25 18:10:36 -0700

    remove comments

--- server/watchHN.js
+++ server/watchHN.js
@@ -4,7 +4,6 @@ var Word = require('./api/word/word.model');
 var startPoint = 8509943; // Arbirtary...
 
 var saveEntry = function(item, id, entry) {
-  console.log('save');
   var article = {
     title: item.title,
     storyUrl: item.url,
@@ -61,11 +60,9 @@ exports.watchData = function(req, res, next) {
         var currentMax = JSON.parse(body);
         if (currentMax > startPoint) {
           var newItemCount = currentMax - startPoint;
-          console.log(newItemCount);
           for(var i=0; i<newItemCount; i++){
             var currentItem = (newItemCount + i);
             searchItem(currentItem);
-            console.log('here');
           }
           startPoint = currentMax;
         }
