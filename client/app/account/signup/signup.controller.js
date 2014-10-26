df5d453f436f9d178cbe853c3b3ec90b94c64d2f
spencer handley <spencerhandley@gmail.com>
2014-10-26 12:40:08 -0700

    fix landing

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -16,7 +16,7 @@ angular.module('mentionrApp')
         })
         .then( function() {
           // Account created, redirect to home
-          $location.path('/');
+          $location.path('/dashboard');
         })
         .catch( function(err) {
           err = err.data;
