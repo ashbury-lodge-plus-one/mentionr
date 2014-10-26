d48edc585e0de64e15730fde90168a9a9e2ecdae
spencer handley <spencerhandley@gmail.com>
2014-10-26 12:53:40 -0700

    fix style

--- server/config/seed.js
+++ server/config/seed.js
@@ -3,27 +3,27 @@
 var User = require('../api/user/user.model');
 var Word = require('../api/word/word.model');
 
-// User.find({}).remove(function() {
-//   User.create({
-//     provider: 'local',
-//     role: 'admin',
-//     name: 'Admin',
-//     email: 'admin@admin.com',
-//     password: 'admin',
-//     words: []
-//   }, function() {
-//       console.log('finished populating users');
-//     }
-//   );
-// });
+User.find({}).remove(function() {
+  User.create({
+    provider: 'local',
+    role: 'admin',
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin',
+    words: []
+  }, function() {
+      console.log('finished populating users');
+    }
+  );
+});
 
-// Word.find({}).remove(function() {
-//   Word.create({
-//     word: 'javascript',
-//     articles: []
+Word.find({}).remove(function() {
+  Word.create({
+    word: 'javascript',
+    articles: []
 
-//   }, function() {
-//       console.log('finished populating words');
-//     }
-//   );
-// });
+  }, function() {
+      console.log('finished populating words');
+    }
+  );
+});
